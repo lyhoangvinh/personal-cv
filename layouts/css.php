000bc43e4bee6c8cb527f1c66937590c12d76d37
Lý Hoàng Vinh <vinh270795@gmail.com>
2019-06-28 14:31:11 +0700

    no message

--- layouts/css.php
+++ layouts/css.php
@@ -9,18 +9,19 @@
     <meta name="description" content="Sunshine - Responsive vCard Template" />
     <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, Sunshine, portfolio" />
     <meta name="author" content="lmtheme" />
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="assets/images/favicon.ico"/>
 
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/css/normalize.css">
-    <link rel="stylesheet" href="assets/css/transition-animations.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="assets/css/lightgallery.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css"/>
+    <link rel="stylesheet" href="assets/css/font-awesome.css"/>
+    <link rel="stylesheet" href="assets/css/normalize.css"/>
+    <link rel="stylesheet" href="assets/css/transition-animations.css"/>
+    <link rel="stylesheet" href="assets/css/animate.css"/>
+    <link rel="stylesheet" href="assets/css/lightgallery.css" type="text/css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
-    <link rel="stylesheet" href="assets/css/main.css" type="text/css">
-    <link rel="stylesheet" href="assets/css/my-style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/main.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/my-style.css" type="text/css"/>
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:300,400,700">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic&subset=latin,latin-ext,cyrillic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:300,400,700"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic&subset=latin,latin-ext,cyrillic"/>
+	
 </head>
